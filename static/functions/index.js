16d1e7ec9b3ed108c5b531213bd15269fc05446e
200ug <a@dyyni.org>
2026-01-31 13:12:59 +0200

    feat: automatic worker deployment (experimental)

---
+++ static/functions/index.js
@@ -0,0 +1,18 @@
+export async function onRequest(context) {
+    const ua = context.request.headers.get("user-agent") || ""
+    if (!/w3m|lynx|links|elinks|curl|wget/.test(ua.toLowerCase())) {
+      return context.next();
+    }
+    const resp = await context.next()
+    return new HTMLRewriter()
+        .on("header img", {
+            element(el) {
+                el.replace(`<pre>
+ ___/ /_ ____ _____  (_)___  _______ _
+/ _  / // / // / _ \\/ // _ \\/ __/ _ \`/
+\\_,_/\\_, /\\_, /_//_/_(_)___/_/  \\_, / 
+    /___//___/                 /___/  
+</pre>`, {html: true})
+            }
+    }).transform(resp)
+}

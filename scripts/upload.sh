43d9479c86b9614f4f4a4418066e0cfb83b3ca25
2u <a@dyyni.org>
2026-05-18 20:44:50 +0300

    feat: rsync wrapper upload script

---
+++ scripts/upload.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+    echo "usage: $0 username@host"
+    exit 1
+fi
+
+DEST="$1"
+REMOTE_DIR="~/services/dyyni_blackbox"
+
+rsync -avz --relative --exclude='bin/' \
+    go/ \
+    scripts/deploy_backend.sh \
+    "$DEST:$REMOTE_DIR/"
